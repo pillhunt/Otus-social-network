7d5a10ca740db2d2f867115118b68a577c9f3511
Квасов Вадим <Kvasov@lanit.ru>
2024-06-28 12:07:44 +0300

    Исправления по репликации

--- Sql_init_scripts/init_tables.sql
+++ Sql_init_scripts/init_tables.sql
@@ -2,8 +2,23 @@ SET search_path TO snhwdb, public;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+CREATE SEQUENCE IF NOT EXISTS public.sn_user_info_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE SEQUENCE IF NOT EXISTS public.sn_user_sessions_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
 CREATE TABLE IF NOT EXISTS public.sn_user_info
 (
+    id bigint NOT NULL DEFAULT nextval('sn_user_info_id_seq'::regclass),
     user_id uuid NOT NULL,
     user_questionnaire_id text COLLATE pg_catalog."default",
     user_name text COLLATE pg_catalog."default",
@@ -26,6 +41,12 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.sn_user_info
 OWNER to snhwdb;
 
+ALTER SEQUENCE public.sn_user_info_id_seq
+    OWNED BY public.sn_user_info.id;
+
+ALTER SEQUENCE public.sn_user_info_id_seq
+    OWNER TO snhwdb;
+
 -- Index: sn_userinfo_id_fname_sname_index
 
 -- DROP INDEX IF EXISTS public.sn_userinfo_id_fname_sname_index;
@@ -37,20 +58,26 @@ CREATE INDEX IF NOT EXISTS sn_user_info_id_fname_sname_index
     WITH (deduplicate_items=True)
     TABLESPACE pg_default;
 
-
 CREATE TABLE IF NOT EXISTS public.sn_user_sessions
 (
+    id bigint NOT NULL DEFAULT nextval('sn_user_sessions_id_seq'::regclass),
     user_session_id uuid NOT NULL,
     user_id uuid NOT NULL,
     user_session_created timestamp without time zone NOT NULL,
     user_session_duration integer NOT NULL,
     user_auth_token text COLLATE pg_catalog."default" NOT NULL,
     user_session_status boolean NOT NULL DEFAULT true,
-    CONSTRAINT sn_user_sessions_pkey PRIMARY KEY (user_session_id),
-	CONSTRAINT sn_user_sessions_unique UNIQUE (user_session_id)
+    CONSTRAINT sn_user_sessions_pkey PRIMARY KEY (user_session_id)
 )
+
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.sn_user_sessions
 OWNER to snhwdb;
+
+ALTER SEQUENCE public.sn_user_sessions_id_seq
+    OWNED BY public.sn_user_sessions.id;
+
+ALTER SEQUENCE public.sn_user_sessions_id_seq
+    OWNER TO snhwdb;
